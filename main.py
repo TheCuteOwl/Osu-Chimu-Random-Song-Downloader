cab825fe548420f0b66931aeb09fa68bf1bc7ea9
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-03-19 16:39:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
 import random
 import os
+import Launchall
+from Launchall import main
+def end():
+    Choice = input('''Installed, Do you want to import all the maps on osu?
+          [1] Yes (Will import and after that Quit) (You need to have osu already launched)
+          [2] No (Quit)
+          ->''')
+    if Choice == '1':
+        Launchall.main()
+        quit()
+    else:
+        quit()
+
 
 def main():
     if not os.path.exists('OsuSong'):
@@ -8,32 +21,52 @@ def main():
     
     min_star = float(input('Enter the minimum star rating: '))
     max_star = float(input('Enter the maximum star rating: '))
-    print(f'Downloading, please be patient (This can take some time)')
+    First = int(input('How many songs do you want to install? '))
+    Debug = input("""Do you want to get more information (Beatmaps that's are not downloaded and some other stuff)*
     
+    [1] Yes
+    [2] No 
+    ->""")
+    print('Downloading, please be patient (This can take some time)')
+    Second = 0
+
     while True:
+        if First == Second:
+            end()
+        else:
+            pass
         random_number = str(random.randint(100000, 160000))
         url = f"https://api.chimu.moe/v1/download/{random_number}?n=1"
+        if Debug == '1':
+            print('Generate new link...')
 
         beatmap_url = f"https://api.chimu.moe/v1/map/{random_number}"
         response = requests.get(beatmap_url)
+
+        if Debug == '1':
+            print('Checking the API if the map exist..')
+
         try:
             data = response.json()
             star_rating = data["DifficultyRating"]
             song_name = data["OsuFile"]
         except KeyError:
             continue
-        if star_rating < min_star and star_rating > max_star:
+        if star_rating < min_star or star_rating > max_star:
             continue
+        
 
         song_name = song_name.replace(".", "_")
         star_rating = f"{star_rating}".replace(".","")
 
-        filename1 = f"{star_rating} TheCuteOwl | Github {song_name}.osz"
+        filename1 = f"{star_rating} Apophis {song_name}.osz"
         if os.path.isfile(os.path.join('OsuSong', filename1)):
             continue
 
         try:
             response = requests.get(url, stream=True)
+            if Debug == '1':
+                print('Map founded...')
             response.raise_for_status() 
             filename2 = os.path.join('OsuSong', filename1)
             with open(filename2, "wb") as f:
@@ -42,6 +75,7 @@ def main():
                         f.write(chunk)
 
             print(f"Downloaded: {filename1} in OsuSong Folder")
+            Second += 1
 
         except:
             pass
